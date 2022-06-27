57286f45fa2b77fe3b512edb60d10feff7f6da32
usmanmehmood-12 <usmanmehmood12@gmail.com>
2022-06-27 13:52:21 +0500

    Add project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
 import './App.css';
+import List from './List';
 
 function App() {
+  const [number, setNumber] = useState(1)
+  const [dark, setDark] = useState(false)
+  const getItems = useCallback(() => {
+    return [number, number + 1, number + 2]
+  }, [number])
+  const theme = {
+    backgroundColor: dark ? "#333" : "#FFF",
+    color: dark ? "#FFF" : "#333",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={theme}>
+      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+      <button onClick={() => setDark(prevDark => !prevDark)}>Toggle Theme</button>
+      <List getItems={getItems} />
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+
+function List({ getItems }) {
+    const [items, setItems] = useState([])
+    useEffect(() => {
+        setItems(getItems())
+        console.log('Updating Items')
+
+    }, [getItems])
+
+    return (
+        items.map(item => <div key={item}>{item}</div>)
+    )
+}
+
+export default List
